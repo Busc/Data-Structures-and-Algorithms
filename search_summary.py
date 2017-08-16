25ec5696a0d7dd0d3fe071f1943fb60677e31bfe
Busc <762521772@qq.com>
2017-08-16 17:49:26 +0800

    二分查找(递归+非递归)

--- search_summary.py
+++ search_summary.py
@@ -6,12 +6,13 @@
 # 基本思想：按比例逐步缩小需要考虑的数据范围，以快速逼近需要找的数据
 # 时间复杂度：O(logn)
 
-def binary_search(random_list, item):
+def binarySearch(random_list, item):
     first_ind, last_ind = 0, len(random_list)-1  # 初始时考虑的区间是整个序列
     item_index = None
     while first_ind <= last_ind:
         # 取待考察区间的中间项索引
-        mid_index = (first_ind + last_ind)//2
+        # mid_index = (first_ind + last_ind)//2 (overflow)
+        mid_index = first_ind + (last_ind - first_ind) // 2
         if random_list[mid_index] == item:
             item_index = mid_index
             break
@@ -22,10 +23,26 @@ def binary_search(random_list, item):
                 last_ind = mid_index - 1
     return item_index
 
+def binarySearch_rec(random_list, item, first_ind, last_ind):
+    item_index = None
+    if first_ind <= last_ind:
+        mid_index = first_ind + (last_ind - first_ind) // 2
+        if random_list[mid_index] == item:
+            item_index = mid_index
+        elif random_list[mid_index] < item:
+           return binarySearch_rec(random_list, item, mid_index+1, last_ind)
+        else:
+            return binarySearch_rec(random_list, item, first_ind, mid_index-1)
+    return item_index
+
 #################测试用例#################
 random_list = [0, 1, 2, 8, 13, 17, 19, 32, 42, 42]
-print(binary_search(random_list, 42))
-print(binary_search(random_list, 3))
-print(binary_search(random_list, 13))
 
+print(binarySearch(random_list, 42))
+print(binarySearch(random_list, 3))
+print(binarySearch(random_list, 13))
+
+print(binarySearch_rec(random_list, 42, 0, len(random_list)-1))
+print(binarySearch_rec(random_list, 3, 0, len(random_list)-1))
+print(binarySearch_rec(random_list, 13, 0, len(random_list)-1))
 
